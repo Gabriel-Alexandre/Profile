aca072c4e70c5ea31d6f66bd719322826681a4ff
Gabriel-Alexandre <biellalex86@gmail.com>
2026-06-22 18:05:07 -0300

    feat: add GAX TECH company reference to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { socialLinks } from "../constants";
+import gaxLogo from "../assets/images/gaxtech-logo.png";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -13,6 +14,16 @@ const Footer = () => {
         <p>
           © {currentYear} <strong>Gabriel Carvalho</strong>. All rights reserved.
         </p>
+
+        <a
+          href='https://www.gaxtech.org/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='flex items-center gap-2 opacity-70 hover:opacity-100 transition-opacity'
+        >
+          <img src={gaxLogo} alt='GAX TECH' className='w-6 h-6 object-contain' />
+          <span className='text-sm text-slate-500'>GAX TECH</span>
+        </a>
 
         <div className='flex gap-3 justify-center items-center'>
           {socialLinks.map((link) => (
